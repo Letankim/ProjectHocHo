a8b308dc5c476de5162561dea4b31d3bac48ce77
Letankim <89767092+Letankim@users.noreply.github.com>
2023-11-28 23:11:58 +0700

    Upload file validation

---
+++ assets/js/validator.js
@@ -0,0 +1,71 @@
+
+function validation(inputsToValidate, submitBtn) {
+// for each item in array input check when blur and when enter in input again clear show error
+    inputsToValidate.forEach(function (item) {
+        item.element.addEventListener('blur', function () {
+            checkInput(item);
+        })
+        item.element.addEventListener('input', function () {
+            const parentNode = item.element.parentElement;
+            parentNode.querySelector('.message_error').innerHTML = "";
+        })
+    })
+
+// check error of element if no match with regex call function show error and return false
+// else return true and call function show success
+    function checkInput(item) {
+        let messageError = '';
+        if(item.type === "text") {
+            if(item.element.value === "") {
+                messageError = "Trường này không được để trống";
+            } else
+            if (!item.element.value.match(item.regex)) {
+                messageError = item.message;
+            } else {
+                messageError = "";
+            }
+        } else if(item.type === "image") {
+
+        } else if(item.type === "radio") {
+
+        } else if(item.type === "checkbox") {
+
+        }
+        if (messageError.trim().length != 0) {
+            showErrorMessage(item.element, messageError);
+            return false;
+        } else {
+            handleSuccess(item.element);
+            return true;
+        }
+    }
+
+//  function show message on each input when have a error
+    function showErrorMessage(element, message) {
+        const parentNode = element.parentElement;
+        parentNode.classList.add("invalid");
+        parentNode.querySelector('.message_error').style.display = 'block';
+        parentNode.querySelector('.message_error').innerHTML = message;
+    }
+//  function handle message when input is valid
+    function handleSuccess(element) {
+        const parentNode = element.parentElement;
+        parentNode.classList.remove("invalid");
+        parentNode.querySelector('.message_error').style.display = 'none';
+        parentNode.querySelector('.message_error').innerHTML = "";
+    }
+//  handle submit event check if all input pass. send data else not send
+
+    submitBtn.addEventListener('click', function (e) {
+        let isValid = true;
+        inputsToValidate.forEach(function (item) {
+            if (!checkInput(item)) {
+                isValid = false;
+            }
+        })
+        if (!isValid) {
+            e.preventDefault();
+        }
+    })
+}
+
